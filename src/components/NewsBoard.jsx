caeb7ac098163541d1d6b389fbacd6f66522bd55
Sahil <118194056+Sahilll94@users.noreply.github.com>
2024-10-21 00:41:38 +0530

    Update NewsBoard.jsx

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -35,7 +35,8 @@ const NewsBoard = ({ category }) => {
       </h2>
 
       {loading && <p className="text-center">Loading news...</p>} {/* Loading state */}
-      {error && <p className="text-center text-danger">Failed to load news as We did not have The Companies premium Plan which gives us allowance to fetch the news in the production mode.</p>} {/* Error handling */}
+      {error && <p className="text-center text-danger">Failed to load news as We did not have The Companies premium Plan which gives us allowance to fetch the news in the production mode. But wait, if you are a developer then simply refer the below documentation to run this project locally on your machine
+      <a href="www.google.com" target="_blank">Click here!</a></p>} {/* Error handling */}
 
       {!loading && !error && (
         <div className='d-flex flex-wrap justify-content-center'>
